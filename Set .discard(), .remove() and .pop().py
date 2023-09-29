f6e4c8c7b84de1f2b302d452f97016a40607f815
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-09-29 21:34:30 +0530

    Create Set .discard(), .remove() and .pop().py

---
+++ Set .discard(), .remove() and .pop().py
@@ -0,0 +1,15 @@
+n = int(input())
+s = set(map(int, input().split()))
+
+N = int(input())
+
+for _ in range(N):
+    command = input().split()
+    if command[0] == 'pop':
+        s.pop()
+    elif command[0] == 'remove':
+        s.remove(int(command[1]))
+    elif command[0] == 'discard':
+        s.discard(int(command[1]))
+
+print(sum(s))
