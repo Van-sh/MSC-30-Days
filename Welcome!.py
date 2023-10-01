b3261bdd26b492d4f60f9d3a7081586fed00367f
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-10-01 20:56:20 +0530

    Create Welcome!.py

---
+++ Welcome!.py
@@ -0,0 +1,23 @@
+def greet(language):
+    database = { "english":"Welcome"
+, "czech": "Vitejte"
+, "danish": "Velkomst"
+, "dutch": "Welkom"
+, "estonian": "Tere tulemast"
+, "finnish": "Tervetuloa"
+, "flemish": "Welgekomen"
+, "french": "Bienvenue"
+, "german": "Willkommen"
+, "irish": "Failte"
+, "italian": "Benvenuto"
+, "latvian": "Gaidits"
+, "lithuanian": "Laukiamas"
+, "polish":"Witamy"
+, "spanish": "Bienvenido"
+, "swedish": "Valkommen"
+,"welsh": "Croeso"
+}
+    if language in database:
+        return database[language]
+    else:
+        return database['english']
