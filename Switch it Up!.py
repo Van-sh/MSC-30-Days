89143d303dce5e30a1f5c06c700074024bf57d16
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-10-03 21:35:13 +0530

    Create Switch it Up!.py

---
+++ Switch it Up!.py
@@ -0,0 +1,22 @@
+def switch_it_up(number):
+    match number:
+        case 1:
+            return 'One'
+        case 2:
+            return 'Two'
+        case 3:
+            return 'Three'
+        case 4:
+            return 'Four'
+        case 5:
+            return 'Five'
+        case 6:
+            return 'Six'
+        case 7:
+            return 'Seven'
+        case 8:
+            return 'Eight'
+        case 9:
+            return 'Nine'
+        case 0:
+            return 'Zero'
