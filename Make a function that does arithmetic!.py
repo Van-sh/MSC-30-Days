01ba49862415267aed77354ebdf0e1f18a464fc1
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-10-04 18:36:37 +0530

    Create Make a function that does arithmetic!.py

---
+++ Make a function that does arithmetic!.py
@@ -0,0 +1,10 @@
+def arithmetic(a, b, operator):
+    match operator:
+        case 'add':
+            return a + b
+        case 'subtract':
+            return a - b
+        case 'multiply':
+            return a * b
+        case 'divide':
+            return a / b
