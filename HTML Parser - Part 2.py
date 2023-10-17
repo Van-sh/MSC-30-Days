ff9b95e7f528d3a6151687d0217dd232a75fdcc0
VanshShandilya <72124535+VanshShandilya@users.noreply.github.com>
2023-10-17 17:05:33 +0530

    Create HTML Parser - Part 2.py

---
+++ HTML Parser - Part 2.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if len(data.split('\n')) == 1:
+            print('>>> Single-line Comment')
+        else:
+            print('>>> Multi-line Comment')
+        print(data)
+
+    def handle_data(self, data):
+        if data != '\n':
+            print(f'>>> Data\n{data}')
+
+
+html = ""
+for i in range(int(input())):
+    html += input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
